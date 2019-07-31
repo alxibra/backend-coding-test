31c537948acea2a86929c07a8793ddecd2f96572
alx <alxibra@gmail.com>
2019-07-31 09:32:57 +0700

    add option to winston js

--- config/winston.js
+++ config/winston.js
@@ -1,2 +1,20 @@
 var appRoot = require('app-root-path');
 var winston = require('winston');
+
+var options = {
+  file: {
+    level: 'info',
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: false,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
