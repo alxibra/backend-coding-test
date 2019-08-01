f23376b2f7d37f3e8a5d1d0aa665ff57781b700b
alx <alxibra@gmail.com>
2019-08-01 08:05:49 +0700

    create spec for driver name

--- test/api.test.js
+++ test/api.test.js
@@ -123,6 +123,34 @@ describe('API tests', () => {
         });
     });
 
+    describe('POST /rides with driver name', () => {
+        it('should return created content', (done) => {
+            var body = {
+                  start_lat: 90,
+                  start_long: 180,
+                  end_lat: 90,
+                  end_long: 180,
+                  rider_name: "check",
+                  driver_name: "",
+                  driver_vehicle: "test"
+              }
+            var expected_response = function(err, res) {
+              if (err) {
+               return done(err);
+              }
+              const content = res.body
+              expect(content.error_code).to.be.equal('VALIDATION_ERROR');
+              expect(content.message).to.be.equal("Driver name must be a non empty string");
+              return done();
+            }
+            request(app)
+                .post('/rides')
+                .send(body)
+                .set('Accept', 'application/json')
+                .expect(200)
+                .end(expected_response);
+        });
+    });
     describe('POST /rides with rider name', () => {
         it('should return created content', (done) => {
             var body = {
