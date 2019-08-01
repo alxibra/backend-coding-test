48997467a04ccb6a7a1596dd084fa05c6ada30f5
alx <alxibra@gmail.com>
2019-08-01 20:39:17 +0700

    create validation

--- src/ride.js
+++ src/ride.js
@@ -1,4 +1,5 @@
 const mapper = require('./ride/mapper.js')
+const validation = require('./ride/validation.js')
 
 const show = (req, res, db) => {
   mapper.show(req, res, db)
@@ -8,91 +9,10 @@ const index = (req, res, db) => {
   mapper.index(req, res, db);
 }
 
-const readResponse = (error, rows) => {
-  let response;
-  if (error) {
-    response = serverErrorResponse();
-  } else if (rows.length === 0) {
-    response = notFoundResponse();
-  } else {
-    response = rows;
-  }
-
-  return response;
-};
-
-const serverErrorResponse = () => ({ error_code: 'SERVER_ERROR', message: 'Unknown error_code' });
-
-const notFoundResponse = () => ({ error_code: 'RIDES_NOT_FOUND_ERROR', message: 'Could not find any rides' });
-
-const isValidStart = (lat, lon) => lat < -90 || lat > 90 || lon < -180 || lon > 180;
-const isValidEnd = (lat, lon) => (lat < -90 || lat > 90 || lon < -180 || lon > 180);
-const isValidName = name => (typeof name !== 'string' || name.length < 1);
-const invalidStartResponse = () => ({
-  error_code: 'VALIDATION_ERROR',
-  message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
-});
-
-const invalidEndResponse = () => ({
-  error_code: 'VALIDATION_ERROR',
-  message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
-});
-const invalidRiderNameResponse = () => ({
-  error_code: 'VALIDATION_ERROR',
-  message: 'Rider name must be a non empty string',
-});
-
-const invalidDriverNameResponse = () => ({
-  error_code: 'VALIDATION_ERROR',
-  message: 'Driver name must be a non empty string',
-});
-
-const invalidDriverVehicleResponse = () => ({
-  error_code: 'VALIDATION_ERROR',
-  message: 'Driver Vehicle must be a non empty string',
-});
-
-const mapRequestBody = req => (
-  {
-    startLatitude: Number(req.body.start_lat),
-    startLongitude: Number(req.body.start_long),
-    endLatitude: Number(req.body.end_lat),
-    endLongitude: Number(req.body.end_long),
-    riderName: req.body.rider_name,
-    driverName: req.body.driver_name,
-    driverVehicle: req.body.driver_vehicle,
-  }
-);
-
-const validationResponse = (body) => {
-  if (isValidStart(body.startLatitude, body.startLongitude)) {
-    return { valid: false, response: invalidStartResponse() };
-  }
-
-  if (isValidEnd(body.endLatitude, body.endLongitude)) {
-    return { valid: false, response: invalidEndResponse() };
-  }
-
-  if (isValidName(body.riderName)) {
-    return { valid: false, response: invalidRiderNameResponse() };
-  }
-
-  if (isValidName(body.driverName)) {
-    return { valid: false, response: invalidDriverNameResponse() };
-  }
-
-  if (isValidName(body.driverVehicle)) {
-    return { valid: false, response: invalidDriverVehicleResponse() };
-  }
-
-  return { valid: true };
-};
-
 const create = (req, res, db) => {
-  const body = mapRequestBody(req);
-
-  if (!validationResponse(body).valid) {
-    return res.send(validationResponse(body).response);
+  var validationResponse = validation.validate(req);
+  if (!validationResponse.valid) {
+    return res.send(validationResponse.response);
   }
   return mapper.create(req, res, db);
 }
