c4df8dd598ebb4188d1b47709985500cfb494cd3
alx <alxibra@gmail.com>
2019-08-01 22:45:55 +0700

    implement async

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const asyncMiddleware = require('./utils/async_middleware.js');
 const express = require('express');
 const bodyParser = require('body-parser');
 const ride = require('./ride.js');
@@ -6,19 +7,19 @@ const jsonParser = bodyParser.json();
 const logger = require('../config/winston.js');
 
 module.exports = (db) => {
-  app.get('/health', (req, res) => res.send('Healthy'));
+  app.get('/health', asyncMiddleware(async (req, res, next) => res.send('Healthy')));
 
-  app.post('/rides', jsonParser, (req, res) => {
+  app.post('/rides', jsonParser, asyncMiddleware(async(req, res, next) => {
     ride.create(req, res, db);
-  });
+  }));
 
-  app.get('/rides', (req, res) => {
+  app.get('/rides', asyncMiddleware(async(req, res, next) => {
     ride.index(req, res, db);
-  });
+  }));
 
-  app.get('/rides/:id', (req, res) => {
+  app.get('/rides/:id', asyncMiddleware(async(req, res, next) => {
     ride.show(req, res, db);
-  });
+  }));
 
   return app;
 };
