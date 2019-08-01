dc79a8043ff1c407d8035e0f4cae26a490d7c5ca
alx <alxibra@gmail.com>
2019-08-01 08:01:04 +0700

    create spec for invalid rider name

--- test/api.test.js
+++ test/api.test.js
@@ -122,4 +122,33 @@ describe('API tests', () => {
                 .end(expected_response);
         });
     });
+
+    describe('POST /rides with rider name', () => {
+        it('should return created content', (done) => {
+            var body = {
+                  start_lat: 90,
+                  start_long: 180,
+                  end_lat: 90,
+                  end_long: 180,
+                  rider_name: "",
+                  driver_name: "alex",
+                  driver_vehicle: "test"
+              }
+            var expected_response = function(err, res) {
+              if (err) {
+               return done(err);
+              }
+              const content = res.body
+              expect(content.error_code).to.be.equal('VALIDATION_ERROR');
+              expect(content.message).to.be.equal("Rider name must be a non empty string");
+              return done();
+            }
+            request(app)
+                .post('/rides')
+                .send(body)
+                .set('Accept', 'application/json')
+                .expect(200)
+                .end(expected_response);
+        });
+    });
 });
