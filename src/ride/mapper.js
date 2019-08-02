2ca5c58caaf7a8e8574c79001983448236feb23d
alx <alxibra@gmail.com>
2019-08-02 09:31:50 +0700

    move response to module

--- src/ride/mapper.js
+++ src/ride/mapper.js
@@ -1,20 +1,5 @@
 const pageParams = require('../page_params.js');
-
-const serverErrorResponse = () => ({ error_code: 'SERVER_ERROR', message: 'Unknown error_code' });
-const notFoundResponse = () => ({ error_code: 'RIDES_NOT_FOUND_ERROR', message: 'Could not find any rides' });
-const readResponse = (error, rows) => {
-  let response;
-  if (error) {
-    response = serverErrorResponse();
-  } else if (rows.length === 0) {
-    response = notFoundResponse();
-  } else {
-    response = rows;
-  }
-
-  return response;
-};
-
+const response = require('./response.js');
 
 const values = req => (
   [
@@ -31,24 +16,24 @@ const values = req => (
 const privateShow = (id, db, res) => {
   db.all('SELECT * FROM Rides WHERE rideID = ?', id, (error, rows) => {
     if (error) {
-      return res.send(serverErrorResponse());
+      return res.send(response.serverError());
     }
     return res.send(rows);
   });
 };
 
 const index = (req, res, db) => {
-  db.all('SELECT * FROM Rides LIMIT ?, ?', pageParams(req), (err, rows) => res.send(readResponse(err, rows)));
+  db.all('SELECT * FROM Rides LIMIT ?, ?', pageParams(req), (err, rows) => res.send(response.read(err, rows)));
 };
 
 const show = (req, res, db) => {
-  db.all('SELECT * FROM Rides WHERE rideID= ?', [req.params.id], (err, rows) => res.send(readResponse(err, rows)));
+  db.all('SELECT * FROM Rides WHERE rideID= ?', [req.params.id], (err, rows) => res.send(response.read(err, rows)));
 };
 
 const create = (req, res, db) => {
   db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values(req), function (err) {
     if (err) {
-      return res.send(serverErrorResponse());
+      return res.send(response.serverError());
     }
     return privateShow(this.lastID, db, res);
   });
