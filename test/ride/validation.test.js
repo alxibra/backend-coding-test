e57b71e3b33b98ff14c9f1b2d24e75adc2a1812a
alx <alxibra@gmail.com>
2019-08-02 08:24:42 +0700

    create spec for invalid start

--- test/ride/validation.test.js
+++ test/ride/validation.test.js
@@ -18,4 +18,44 @@ describe('ride validation', function() {
   it('must success', function(){
     expect(validation.validate(req)).to.eql({valid: true});
   });
+
+  describe('when start is not valid', function(){
+    var req = {
+      body: {
+        start_lat: 91,
+        start_lon: 180,
+        end_lat: 90,
+        end_long: 180,
+        rider_name: 'alex',
+        driver_name: 'alex',
+        driver_vehicle: 'test'
+      }
+    }
+    var response = {
+      error_code: 'VALIDATION_ERROR',
+      message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
+    }
+    it('must not valid', function(){
+      expect(validation.validate(req).valid).to.eql(false);
+    });
+    it('must have message', function(){
+      expect(validation.validate(req).response).to.eql(response);
+    });
+  })
+  describe('when end is not valid', function(){
+    var req = {
+      body: {
+        start_lat: 90,
+        start_lon: 180,
+        end_lat: 91,
+        end_long: 180,
+        rider_name: 'alex',
+        driver_name: 'alex',
+        driver_vehicle: 'test'
+      }
+    }
+    it('must not valid', function(){
+      expect(validation.validate(req).valid).to.eql(false);
+    });
+  })
 });
